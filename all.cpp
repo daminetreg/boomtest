bc67ed944787b59f988bd8dc9540e70b340f04c4
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2022-09-14 12:48:18 -0600

    :wrench: fix the json version.

--- all.cpp
+++ all.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <iostream>
+#include <iostream>
 //#include <boost/filesystem.hpp>
 #include <all.hpp>
 void banana() {
   std::cout << "boom" /*<< boost::filesystem::current_path()*/ << std::endl;
+  std::cout << "boom" /*<< boost::filesystem::current_path()*/ << std::endl;
+  function();
   function();
   function();
   function();
